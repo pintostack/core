8c505f67ddc87b8710333eb23289df8ebe6db97f
yvlasov <yvlasov@pytn.ru>
2016-01-12 15:26:17 +0300

    AWS and DO oops forgot to add provision.sh

--- provision.sh
+++ provision.sh
@@ -8,85 +8,97 @@ function print_usage (){
 }
 
 function provision_digital_ocean() {
-    source do.source
+    source source.digital_ocean
+    #__do_token
+    #__do_image
+    #__do_region
+    #__do_size
+    #__ssh_key_path
 
-    for (( i=1; i <= $1; i++ )) do
-        echo "Provisioning master $i"
-        ansible-playbook -i digital_ocean.py provision/do-master.yaml -e master_id=$i -e ssh_key=\""$SSH_KEY_ID"\" -e size=$SIZE -e location=$LOCATION -e image=$IMAGE_NAME
-    done
+    #Vagrantfile
+    sed -i.bak "s|__masters_count|${1}|g;s|__slaves_count|${2}|g;s|__do_token|${DO_TOKEN}|g;s|__do_image|${DO_IMAGE}|g;s|__do_region|${DO_REGION}|g;s|__do_size|${DO_SIZE}|g;s|__ssh_key_path|${SSH_KEY_FILE}|g;" Vagrantfile
+    
+    vagrant up --provider=digital_ocean
 
-    for (( i=1; i <= $2; i++ ))  do
-        echo "Provisioning slave $i" 
-        ansible-playbook -i digital_ocean.py provision/do-slave.yaml -e slave_id=$i -e ssh_key=\""$SSH_KEY_ID"\" -e size=$SIZE -e location=$LOCATION -e image=$IMAGE_NAME
-    done
-
-    ansible-playbook -i digital_ocean.py provision/do-docker-registry.yaml -e ssh_key=\""$SSH_KEY_ID"\" -e size=$SIZE -e location=$LOCATION -e image=$IMAGE_NAME
-
-    sed -i.bak "s|__inv_file|digital_ocean.py|g;s|__adds|-vvv --private-key $SSH_KEY_FILE|g;s|__iface|eth1|g;s|__username|root|g" bootstrap-*.sh
-    sed -i.bak "s|__inv_file|digital_ocean.py|g;s|__adds|-vvv --private-key $SSH_KEY_FILE|g;s|__iface|eth1|g;s|__username|root|g" docker-build.sh
-    sed -i.bak "s|__inv_file|digital_ocean.py|g;s|__adds|-vvv --private-key $SSH_KEY_FILE|g;s|__iface|eth1|g;s|__username|root|g" marathon-deploy.sh
+    #bootstrap-*.sh
+    sed -i.bak "s|__inv_file|${INVENTORY_FILE}|g;s|__adds|-vvv --private-key $SSH_KEY_FILE|g;s|__iface|eth1|g;s|__username|root|g" bootstrap-*.sh
+    #docker-build.sh
+    sed -i.bak "s|__inv_file|${INVENTORY_FILE}|g;s|__adds|-vvv --private-key $SSH_KEY_FILE|g;s|__iface|eth1|g;s|__username|root|g" docker-build.sh
+    #marathon-deploy.sh
+    sed -i.bak "s|__inv_file|${INVENTORY_FILE}|g;s|__adds|-vvv --private-key $SSH_KEY_FILE|g;s|__iface|eth1|g;s|__username|root|g" marathon-deploy.sh
 }
 
-function provision_ec2() {
-    source ec2.source
-    cp provision/ec2.ini.tmpl ./ec2.ini
-    sed -i.bak 's/__aws_region/'${AWS_REGION}'/g' ec2.ini
+function provision_aws() {
+    source source.aws
+    #__ssh_key_path
+    #__aws_key_id
+    #__aws_access_key
+    #__aws_keypair_name
+    #__aws_ami
+    #__aws_instance_type
+    #__aws_region
+    #__aws_security_groups
+    #__aws_username
 
-    for ((i=1; i<=$1; i++)); do
-        echo "Provisioning master $i"
-        ansible-playbook -i ec2.py provision/ec2-master.yaml -e master_id=$i -e vpc_subnet_id=$AWS_VPC_SUBNET_ID -e region=$AWS_REGION -e master_instance=$AWS_MASTER_INSTANCE -e ami=$AWS_AMI -e key_name=$AWS_SSH_KEY_NAME
-    done
+    #Vagrantfile
+    sed -i.bak "s|__masters_count|$1|g;s|__slaves_count|$2|g;s|__aws_key_id|$AWS_KEY_ID|g;s|__aws_access_key|$AWS_ACCESS_KEY|g;s|__aws_keypair_name|$AWS_KEYPAIR_NAME|g;s|__aws_ami|$AWS_AMI|g;s|__aws_instance_type|$AWS_INSTANCE_TYPE|g;s|__aws_region|$AWS_REGION|g;s|'__aws_security_groups'|[$AWS_SECURITY_GROUPS]|g;s|__aws_username|${AWS_USERNAME}|g;s|__ssh_key_path|${SSH_KEY_FILE}|g;" Vagrantfile
 
-    for ((i=1; i<=$2; i++)); do
-        echo "Provisioning slave $i" 
-        ansible-playbook -i ec2.py provision/ec2-slave.yaml -e slave_id=$i -e ami=$AWS_AMI -e vpc_subnet_id=$AWS_VPC_SUBNET_ID -e slave_instance=$AWS_SLAVE_INSTANCE -e region=$AWS_REGION -e key_name=$AWS_SSH_KEY_NAME
-    done
+    vagrant up --provider=aws
 
-    ansible-playbook -i ec2.py provision/ec2-docker-registry.yaml -e vpc_subnet_id=$AWS_VPC_SUBNET_ID -e ami=$AWS_AMI -e docker_instance=$AWS_DOCKER_INSTANCE -e region=$AWS_REGION -e key_name=$AWS_SSH_KEY_NAME    
-    ./ec2.py --refresh-cache > /dev/null
+    ansible-playbook -i ${INVENTORY_FILE} provision/ec2-hostnames.yaml -e user=${AWS_USERNAME} --private-key $SSH_KEY_FILE
 
-    ansible-playbook -i ec2.py provision/ec2-hostnames.yaml -e user=$BOXUSERNAME --private-key $SSH_KEY_PATH
-
-    ./ec2.py --refresh-cache > /dev/null
-
-    sed -i.bak "s/__inv_file/ec2.py/g;s/__adds/-vvv --private-key $SSH_KEY_FILE/g;s/__iface/eth0/g;s/__username/$BOXUSERNAME/g" bootstrap-*.sh
-    sed -i.bak "s/__inv_file/ec2.py/g;s/__adds/-vvv --private-key $SSH_KEY_FILE/g;s/__iface/eth0/g;s/__username/$BOXUSERNAME/g" docker-build.sh
-    sed -i.bak "s/__inv_file/ec2.py/g;s/__adds/-vvv --private-key $SSH_KEY_FILE/g;s/__iface/eth0/g;s/__username/$BOXUSERNAME/g" marathon-deploy.sh
+    #bootstrap-*.sh
+    sed -i.bak "s|__inv_file|${INVENTORY_FILE}|g;s|__adds|-vvv --private-key $SSH_KEY_FILE|g;s|__iface|eth0|g;s|__username|${AWS_USERNAME}|g" bootstrap-*.sh
+    #docker-build.sh
+    sed -i.bak "s|__inv_file|${INVENTORY_FILE}|g;s|__adds|-vvv --private-key $SSH_KEY_FILE|g;s|__iface|eth0|g;s|__username|${AWS_USERNAME}|g" docker-build.sh
+    #marathon-deploy.sh
+    sed -i.bak "s|__inv_file|${INVENTORY_FILE}|g;s|__adds|-vvv --private-key $SSH_KEY_FILE|g;s|__iface|eth0|g;s|__username|${AWS_USERNAME}|g" marathon-deploy.sh
 }
 
-function provision_vagrant() {
-    cp provision/Vagrantfile ./
-    cp provision/playbook.yml ./
-    sed -i.bak s/__masters_count/$1/g Vagrantfile
-    sed -i.bak s/__slaves_count/$2/g Vagrantfile
-    vagrant up
-    sed -i.bak 's/__inv_file/.vagrant\/provisioners\/ansible\/inventory\/vagrant_ansible_inventory/g;s/__adds/-vvv/g;s/__iface/eth1/g;s/__username/vagrant/g' bootstrap-*.sh
-    sed -i.bak 's/__inv_file/.vagrant\/provisioners\/ansible\/inventory\/vagrant_ansible_inventory/g;s/__adds/-vvv/g;s/__iface/eth1/g;s/__username/vagrant/g' docker-build.sh
-    sed -i.bak 's/__inv_file/.vagrant\/provisioners\/ansible\/inventory\/vagrant_ansible_inventory/g;s/__adds/-vvv/g;s/__iface/eth1/g;s/__username/vagrant/g' marathon-deploy.sh  
+function provision_virtualbox() {
+    source source.virtualbox
+    #__ssh_key_path
+    #__vbox_size
+
+    sed -i.bak "s|__masters_count|${1}|g;s|__slaves_count|${2}|g;s|__vbox_size|${VBOX_SIZE}|g;s|__ssh_key_path|${SSH_KEY_FILE}|g;" Vagrantfile
+
+    echo vagrant up --provider=virtualbox
+
+    #bootstrap-*.sh
+    sed -i.bak "s|__inv_file|${INVENTORY_FILE}|g;s|__adds|-vvv|g;s|__iface|eth1|g;s|__username|vagrant|g" bootstrap-*.sh
+    #docker-build.sh
+    sed -i.bak "s|__inv_file|${INVENTORY_FILE}|g;s|__adds|-vvv|g;s|__iface|eth1|g;s|__username|vagrant|g" docker-build.sh
+    #marathon-deploy.sh
+    sed -i.bak "s|__inv_file|${INVENTORY_FILE}|g;s|__adds|-vvv|g;s|__iface|eth1|g;s|__username|vagrant|g" marathon-deploy.sh  
 }
 
 function provision_azure() {
-    source azure.source
-
-    for ((i=1; i<=$1; i++)); do
-        echo "Provisioning master $i"
-        ansible-playbook -i windows_azure.py $VERBOSITY provision/wa-master.yaml -e location=\""$LOCATION"\" -e username=$BOXUSERNAME -e ports="$MASTER_PORTS" -e password=$PASSWD -e cluster=$CLUSTER -e storage=$STORAGE_NAME -e master_id=$i -e cert_path=$CER_FILE -e pem_file=$PEM_FILE
-    done
-
-    for ((i=1; i<=$2; i++)); do
-        echo "Provisioning slave $i"
-        ansible-playbook -i windows_azure.py $VERBOSITY provision/wa-slave.yaml -e location=\""$LOCATION"\" -e username=$BOXUSERNAME -e ports=$SLAVE_PORTS -e password=$PASSWD -e cluster=$CLUSTER -e storage=$STORAGE_NAME -e slave_id=$i  -e cert_path=$CER_FILE -e pem_file=$PEM_FILE
-
-    done
-
-    ansible-playbook -i windows_azure.py $VERBOSITY provision/wa-docker-registry.yaml -e location=\""$LOCATION"\" -e username=$BOXUSERNAME -e ports=$DOCKER_PORTS -e password=$PASSWD -e cluster=$CLUSTER -e storage=$STORAGE_NAME -e cert_path=$CER_FILE -e pem_file=$PEM_FILE
-
-    ./windows_azure.py --refresh-cache > /dev/null
-
-    sed -i.bak "s|__inv_file|windows_azure.py|g;s|__adds|$VERBOSITY --private-key $PEM_FILE|g;s|__iface|eth0|g;s|__username|$BOXUSERNAME|g" bootstrap-*.sh
-    sed -i.bak "s|__inv_file|windows_azure.py|g;s|__adds|$VERBOSITY --private-key $PEM_FILE|g;s|__iface|eth0|g;s|__username|$BOXUSERNAME|g" docker-build.sh
-    sed -i.bak "s|__inv_file|windows_azure.py|g;s|__adds|$VERBOSITY --private-key $PEM_FILE|g;s|__iface|eth0|g;s|__username|$BOXUSERNAME|g" marathon-deploy.sh
+    source source.azure
+    echo "Temporary unavailable.. We working on this..."
+    exit 1
+#    ansible-playbook -i windows_azure.py $VERBOSITY provision/wa-docker-registry.yaml -e location=\""$LOCATION"\" -e username=$BOXUSERNAME -e ports=$DOCKER_PORTS -e password=$PASSWD -e cluster=$CLUSTER -e storage=$STORAGE_NAME -e cert_path=$CER_FILE -e pem_file=$PEM_FILE
+#    ./windows_azure.py --refresh-cache > /dev/null
+#    sed -i.bak "s|__inv_file|windows_azure.py|g;s|__adds|$VERBOSITY --private-key $PEM_FILE|g;s|__iface|eth0|g;s|__username|$BOXUSERNAME|g" bootstrap-*.sh
+#    sed -i.bak "s|__inv_file|windows_azure.py|g;s|__adds|$VERBOSITY --private-key $PEM_FILE|g;s|__iface|eth0|g;s|__username|$BOXUSERNAME|g" docker-build.sh
+#    sed -i.bak "s|__inv_file|windows_azure.py|g;s|__adds|$VERBOSITY --private-key $PEM_FILE|g;s|__iface|eth0|g;s|__username|$BOXUSERNAME|g" marathon-deploy.sh
 }
+
+function prepare_files() {
+    cd infrastructure
+    rm Vagrantfile
+    rm -rf .vagrant
+    cp provision/Vagrantfile.tmpl ./Vagrantfile
+    cp provision/playbook.yml ./
+
+    rm bootstrap-*.sh
+    rm docker-build.sh
+    rm marathon-deploy.sh
+    cp bootstrap/bootstrap*.sh ./
+    cp bootstrap/docker-build.sh ./
+    cp bootstrap/marathon-deploy.sh ./
+    echo "Ready to provision $MASTER master(s) and $SLAVE slave(s) to $TARGET"
+}
+
 
 # TODO set SOFT_CLOUD_PROVIDER
 
@@ -94,16 +106,6 @@ if [ $# = 0 ]; then
     print_usage
     exit
 fi
-
-cd infrastructure
-
-rm bootstrap-*.sh
-rm docker-build.sh
-rm marathon-deploy.sh
-
-cp bootstrap/bootstrap*.sh ./
-cp bootstrap/docker-build.sh ./
-cp bootstrap/marathon-deploy.sh ./
 
 
 while getopts ":-:" optchar
@@ -125,27 +127,45 @@ do
        esac
 done
 
-echo "Ready to provision $MASTER master(s) and $SLAVE slave(s) to $TARGET"
-
-if [ $((MASTER%2)) -eq 0 ]; then
+if [ ${MASTER} -eq 0 ]; then
    echo "Number of master nodes is odd, should be even number."
    print_usage
    exit
 fi
 
+source infrastructure/source.global
+INVENTORY_FILE=".vagrant\/provisioners\/ansible\/inventory\/vagrant_ansible_inventory"
+
+
 case "$TARGET" in
     "do")
-        provision_digital_ocean $MASTER $SLAVE
-        ;;
+        prepare_files
+	provision_digital_ocean $MASTER $SLAVE
+       ;;
+    "digital_ocean")
+        prepare_files
+	provision_digital_ocean $MASTER $SLAVE
+       ;;
     "aws")
-	provision_ec2 $MASTER $SLAVE
-	;;
+        prepare_files
+	provision_aws $MASTER $SLAVE
+	   ;;
+    "ec2")
+        prepare_files
+	provision_aws $MASTER $SLAVE
+       ;;
     "vagrant")
-        provision_vagrant $MASTER $SLAVE
-        ;;
+        prepare_files
+	provision_virtualbox $MASTER $SLAVE
+       ;;
+    "virtualbox")
+        prepare_files
+	provision_virtualbox $MASTER $SLAVE
+       ;;
     "azure")
-        provision_azure $MASTER $SLAVE
-        ;;
+        prepare_files
+	provision_azure $MASTER $SLAVE
+       ;;
     *)
         print_usage
         exit
