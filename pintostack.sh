2e3d72829256950f5e272ba7fc6fc5be1ab45696
yvlasov <yvlasov@pytn.ru>
2016-02-01 16:19:40 +0300

    Fix pintostack.sh to run with non POSIX ENV

--- pintostack.sh
+++ pintostack.sh
@@ -3,9 +3,11 @@ PROVIDER=virtualbox
 if [ $# -eq 1 ]; then
   PROVIDER=$1
 fi
+
+GLOBAL_VARS_LIST="$( set -o posix ; set | cut -f1 -d= )"
+
 if [ -f conf/source.$PROVIDER ]; then
     source conf/source.$PROVIDER
-    echo "$AWS_REGION"
 else
     echo "ERROT: No file: conf/source.$PROVIDER"
     exit 1
@@ -13,6 +15,12 @@ fi
 
 ( set -o posix ; set ) > .env
 
+for each in $GLOBAL_VARS_LIST; do
+	sed -i "/^${each}/d" .env
+done
+
 echo "Provider is ${PROVIDER}"
+echo "Setting Vagrant env..."
+cat .env
 
 vagrant up --provider=${PROVIDER}
