2bc97c4342aa2bd7bc1ae4fb788279d8414e02bb
yvlasov <yvlasov@pytn.ru>
2016-02-01 14:20:27 +0300

    More minor fixes to support tc

--- pintostack.sh
+++ pintostack.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
-set -e
-
 PROVIDER=virtualbox
 if [ $# -eq 1 ]; then
   PROVIDER=$1
 fi
-cat ./conf/source.$PROVIDER | while read line; do
-    declare -x $line
-done
+if [ -f conf/source.$PROVIDER ]; then
+    source conf/source.$PROVIDER
+    echo "$AWS_REGION"
+else
+    echo "ERROT: No file: conf/source.$PROVIDER"
+    exit 1
+fi
+
+( set -o posix ; set ) > .env
+
 echo "Provider is ${PROVIDER}"
-env > .env
-vagrant up --provider=$PROVIDER
