d59673e2d0befb82ed6dc29140d13911ef6f1586
yvlasov <yvlasov@pytn.ru>
2016-03-29 16:04:15 +0300

    Final checks

--- examples/uk-juju/deploy_uk_demo.sh
+++ examples/uk-juju/deploy_uk_demo.sh
@@ -2,7 +2,14 @@
 set -e
 export JUJU_REPOSITORY=$PWD/juju/
 
+juju bootstrap
+
 juju set-env "default-series=trusty"
+
+cd juju
+./build_charms.sh
+cd ..
+
 #juju set-constraints "mem=512M"
 #juju set-constraints 'instance-type=m1.small'
 
@@ -13,14 +20,14 @@ juju expose juju-gui
 
 echo "Stating Pintostack and tutorial components"
 
-if [ ! -f my-config.yaml ]; then
-	echo "File: my-config.yaml do not exist, please copy my-config.yaml.tmpl and edit"
+if [ ! -f config.yaml ]; then
+	echo "File: config.yaml do not exist, please copy config.yaml.tmpl and edit"
 	exit 1
 else
-	echo "File: my-config.yaml found, deploying configuration"
+	echo "File: config.yaml found, deploying configuration"
 fi
 
-juju deploy local:trusty/pintostack --config my-config.yaml --to 0
+juju deploy local:trusty/pintostack --config config.yaml --to 0
 juju deploy local:trusty/hdfsnn4pintostack --to 0
 juju deploy local:trusty/hdfsdn4pintostack --to 0
 juju deploy local:trusty/ipythonnb4pintostack --to 0
